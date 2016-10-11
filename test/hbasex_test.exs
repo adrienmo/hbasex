fc6fdd9f0a530cc438535c67e6d20a4e7e5e8e12
steffel fenix <steffel.fenix@gmail.com>
2016-10-11 13:37:40 +0800

    catch exits and give error

--- test/hbasex_test.exs
+++ test/hbasex_test.exs
@@ -13,43 +13,43 @@ defmodule HbasexTest do
   end
 
   test "put and get" do
-    Hbasex.put("abc", "123", %{"a" => %{"c1" => "efg", "c2" => "hjk"}})
-    result = Hbasex.get("abc", "123")
+    Hbasex.put!("abc", "123", %{"a" => %{"c1" => "efg", "c2" => "hjk"}})
+    result = Hbasex.get!("abc", "123")
     assert result == %{"a" => %{"c1" => "efg", "c2" => "hjk"}}
 
-    result = Hbasex.get("abc", "123", %{"a" => ["c2"]})
+    result = Hbasex.get!("abc", "123", %{"a" => ["c2"]})
     assert result == %{"a" => %{"c2" => "hjk"}}
 
-    result = Hbasex.get("abc", "123", %{"a" => ["random_value"]})
+    result = Hbasex.get!("abc", "123", %{"a" => ["random_value"]})
     assert result == %{}
    end
 
   test "scan" do
-    Hbasex.put("scan_test", "10#10000", %{"a" => %{"value" => "9"}})
-    Hbasex.put("scan_test", "12#10000", %{"a" => %{"value" => "1"}})
-    Hbasex.put("scan_test", "12#10001", %{"a" => %{"value" => "2"}})
-    Hbasex.put("scan_test", "12#10002", %{"a" => %{"value" => "3"}})
-    Hbasex.put("scan_test", "12#10003", %{"a" => %{"value" => "4"}})
-    Hbasex.put("scan_test", "12#同道", %{"a" => %{"value" => "2"}})
-    Hbasex.put("scan_test", "15#10003", %{"a" => %{"value" => "1"}})
+    Hbasex.put!("scan_test", "10#10000", %{"a" => %{"value" => "9"}})
+    Hbasex.put!("scan_test", "12#10000", %{"a" => %{"value" => "1"}})
+    Hbasex.put!("scan_test", "12#10001", %{"a" => %{"value" => "2"}})
+    Hbasex.put!("scan_test", "12#10002", %{"a" => %{"value" => "3"}})
+    Hbasex.put!("scan_test", "12#10003", %{"a" => %{"value" => "4"}})
+    Hbasex.put!("scan_test", "12#同道", %{"a" => %{"value" => "2"}})
+    Hbasex.put!("scan_test", "15#10003", %{"a" => %{"value" => "1"}})
 
-    assert length(Hbasex.scan("scan_test", 10, prefix: "12#")) == 5
-    assert length(Hbasex.scan("scan_test", 2, prefix: "12#")) == 2
-    assert length(Hbasex.scan("scan_test", 10)) == 7
-    assert Hbasex.scan("scan_test", 10, prefix: "15#") == [{"15#10003", %{"a" => %{"value" => "1"}}}]
+    assert length(Hbasex.scan!("scan_test", 10, prefix: "12#")) == 5
+    assert length(Hbasex.scan!("scan_test", 2, prefix: "12#")) == 2
+    assert length(Hbasex.scan!("scan_test", 10)) == 7
+    assert Hbasex.scan!("scan_test", 10, prefix: "15#") == [{"15#10003", %{"a" => %{"value" => "1"}}}]
   end
 
   test "put and delete" do
     for i <- 1..10 do
-      Hbasex.put("delete_test", to_string(i), %{"a" => %{"a" => "1"}})
+      Hbasex.put!("delete_test", to_string(i), %{"a" => %{"a" => "1"}})
     end
-    assert length(Hbasex.scan("delete_test", 10)) == 10
+    assert length(Hbasex.scan!("delete_test", 10)) == 10
 
-    Hbasex.delete("delete_test", "1")
-    assert Hbasex.get("delete_test", "1") == %{}
-    assert Hbasex.get("delete_test", "2") != %{}
+    Hbasex.delete!("delete_test", "1")
+    assert Hbasex.get!("delete_test", "1") == %{}
+    assert Hbasex.get!("delete_test", "2") != %{}
 
-    Hbasex.delete("delete_test", ["2", "3", "4"])
-    assert length(Hbasex.scan("delete_test", 10)) == 6
+    Hbasex.delete!("delete_test", ["2", "3", "4"])
+    assert length(Hbasex.scan!("delete_test", 10)) == 6
   end
 end
